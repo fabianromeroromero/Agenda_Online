cd2bd977e3d58308704042ed40d4e50c6aca24cd
fabianromeroromero <163800699+fabianromeroromero@users.noreply.github.com>
2024-05-13 18:46:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/agenda_online/Pantalla_De_CargaActivity.java
@@ -0,0 +1,45 @@
+package com.example.agenda_online;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Pantalla_De_CargaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pantalla_de_carga);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(2000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(Pantalla_De_CargaActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/agenda_online/RegisterActivity.java
@@ -0,0 +1,169 @@
+package com.example.agenda_online;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edtNombreUser, edtEmailUser, edtPasswordUser, edtPasswordConfir;
+    Button btnNewRegister;
+    TextView txtTengoCuenta;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+
+    String nombre = "", correo = "", password = "", confirmarpassword = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Registar");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        edtNombreUser = findViewById(R.id.edtNombreUser);
+        edtEmailUser = findViewById(R.id.edtEmailUser);
+        edtPasswordUser = findViewById(R.id.edtNombreUser);
+        edtPasswordConfir = findViewById(R.id.edtPasswordConfir);
+        btnNewRegister = findViewById(R.id.btnNewRegister);
+        txtTengoCuenta = findViewById(R.id.txtTengoCuenta);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(RegisterActivity.this);
+        progressDialog.setTitle("Espere por favor");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        btnNewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validarDatos();
+            }
+        });
+
+        txtTengoCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void validarDatos(){
+        nombre = edtNombreUser.getText().toString();
+        correo = edtEmailUser.getText().toString();
+        password = edtPasswordUser.getText().toString();
+        confirmarpassword = edtPasswordConfir.getText().toString();
+
+        if (TextUtils.isEmpty(nombre)){
+            Toast.makeText(this, "Ingrese nombre", Toast.LENGTH_SHORT).show();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            Toast.makeText(this, "Ingrese correo", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Ingrese contraseña", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(confirmarpassword)) {
+            Toast.makeText(this, "Confirme contaseña", Toast.LENGTH_SHORT).show();
+        }
+        else if (!password.equals(confirmarpassword)) {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            crearCuenta();
+        }
+    }
+
+    private void crearCuenta() {
+        progressDialog.setMessage("Creando su cuenta");
+        progressDialog.show();
+
+        //Crear un usuario en Firebase
+        firebaseAuth.createUserWithEmailAndPassword(correo, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        GuardarInformacion();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void GuardarInformacion() {
+        progressDialog.setMessage("Guardondo su informacion");
+        progressDialog.dismiss();
+
+        //Obtener la identificacion de usuario actual
+        String uid= firebaseAuth.getUid();
+
+        HashMap<String, String> Datos = new HashMap<>();
+        Datos.put("uid", uid);
+        Datos.put("correo", correo);
+        Datos.put("nombres", nombre);
+        Datos.put("password", password);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("usuarios");
+        databaseReference.child(uid)
+                .setValue(Datos)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Cuenta creada con exito", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegisterActivity.this, MenuPrincipalActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
