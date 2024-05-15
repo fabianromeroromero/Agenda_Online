5f68dea1486fa9a2f1ee4d4130260cbc5e2624a7
fabianromeroromero <163800699+fabianromeroromero@users.noreply.github.com>
2024-05-14 23:06:29 -0500

    Login listo

--- app/src/main/java/com/example/agenda_online/LoginActivity.java
+++ app/src/main/java/com/example/agenda_online/LoginActivity.java
@@ -1,14 +1,45 @@
 package com.example.agenda_online;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText correoLogin, contrasenaLogin;
+
+    Button btnLogin;
+
+    TextView txtNoTengoCuenta;
+
+    ProgressDialog progressDialog;
+
+    FirebaseAuth firebaseAuth;
+
+    //validar los datos
+    String correo = "", password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +47,93 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        // Configura la barra de acción (ActionBar)
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Login");
+        actionBar.setDisplayHomeAsUpEnabled(true); // Habilita el botón de "Atrás" en la barra de acción
+        actionBar.setDisplayShowHomeEnabled(true); // Habilita mostrar el ícono de la aplicación en la barra de acción
+
+        correoLogin = findViewById(R.id.correoLogin);
+        contrasenaLogin = findViewById(R.id.contrasenaLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtNoTengoCuenta = findViewById(R.id.txtNoTengoCuenta);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("Espere por favor");
+        progressDialog.setCanceledOnTouchOutside(false); // Evita que se cancele al tocar fuera del diálog
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validardatos();
+            }
+        });
+
+        txtNoTengoCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        //Accion del ojo
+        imgButtonPassword.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                onClickTogglePassword();
+            }
+        });
+    }
+
+    private void validardatos() {
+
+        correo = correoLogin.getText().toString();
+        password = contrasenaLogin.getText().toString();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            Toast.makeText(this, "Correo invalido", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Ingrese contraseña", Toast.LENGTH_SHORT).show();
+        } else {
+            LoginDeUsuario();
+        }
+    }
+
+    private void LoginDeUsuario() {
+        progressDialog.setMessage("Iniciando secion ...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(correo, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, MenuPrincipalActivity.class));
+                            Toast.makeText(LoginActivity.this, "Bienvenido(a) "+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Verifique si el correo y consaseña son los correctos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
